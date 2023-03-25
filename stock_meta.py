35e1a30bdec59400e84686e07067b44a9b708e7c
Alex Liang <lianglx.alex@gmail.com>
2023-03-26 00:22:13 +0800

    update at

--- stock_meta.py
+++ stock_meta.py
@@ -17,6 +17,7 @@ cursor.execute("""
         code_name VARCHAR(100)
     )
 """)
+cursor.execute("TRUNCATE TABLE stock_codes")
 lg = bs.login()
 rs = bs.query_all_stock()
 while (rs.error_code == '0') & rs.next():
@@ -24,7 +25,6 @@ while (rs.error_code == '0') & rs.next():
     cursor.execute("""
         INSERT INTO stock_codes (code, tradestatus, code_name) 
         VALUES (%s, %s, %s) 
-        ON DUPLICATE KEY UPDATE code=code
     """, (data[0], data[1], data[2]))
 cnx.commit()
 cursor.close()
