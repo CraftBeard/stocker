ebf4a8e323d8095b55e826d7adaf5eaaba4b22e8
Alex Liang <lianglx.alex@gmail.com>
2023-03-25 21:17:46 +0800

    init

---
+++ stock_prices.py
@@ -0,0 +1,81 @@
+import baostock as bs
+import pandas as pd
+import pymysql
+import sys
+
+stocks = ["sh.600036", "sh.601012", "sz.002594", "sh.688598", "sh.300001", "sh.600519", "sh.600389", "sz.300316"]
+
+# login to mysql
+conn = pymysql.connect(host='localhost', user='stocker', password='2016@uq$tencent', database='stock')
+
+# create mysql table if not exists
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS stock_prices (
+                    date DATE,
+                    code VARCHAR(10),
+                    open FLOAT,
+                    high FLOAT,
+                    low FLOAT,
+                    close FLOAT,
+                    volume FLOAT,
+                    amount FLOAT,
+                    preclose FLOAT,
+                    adjustflag INT,
+                    turn FLOAT,
+                    tradestatus INT,
+                    pctChg FLOAT,
+                    isST INT,
+                    PRIMARY KEY (date, code)
+                )''')
+
+# get stock data from baostock
+lg = bs.login()
+for stock in stocks:
+    rs = bs.query_history_k_data_plus(stock, start_date=sys.argv[1], end_date=sys.argv[2], frequency="d", adjustflag="3")
+    data_list = []
+    while (rs.error_code == '0') & rs.next():
+        data_list.append(rs.get_row_data())
+    result = pd.DataFrame(data_list, columns=rs.fields)
+
+    # insert data into mysql
+    for index, row in result.iterrows():
+        sql = f"""
+        INSERT IGNORE INTO stock_prices (
+            date, 
+            code, 
+            open, 
+            high, 
+            low, 
+            close, 
+            volume, 
+            amount, 
+            preclose, 
+            adjustflag, 
+            turn, 
+            tradestatus, 
+            pctChg, 
+            isST
+        ) VALUES (
+            '{row['date']}', 
+            '{row['code']}', 
+            {row['open']}, 
+            {row['high']}, 
+            {row['low']}, 
+            {row['close']}, 
+            {row['volume']}, 
+            {row['amount']}, 
+            {row['preclose']}, 
+            {row['adjustflag']}, 
+            {row['turn']}, 
+            {row['tradestatus']}, 
+            {row['pctChg']}, 
+            {row['isST']}
+        )
+        """
+        cursor.execute(sql)
+conn.commit()
+
+# logout from baostock and mysql
+bs.logout()
+cursor.close()
+conn.close()
