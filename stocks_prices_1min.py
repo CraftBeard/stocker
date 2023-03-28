b6214b0c5aa9afc1b57859ea1d8472bba39c49d4
Alex Liang <lianglx.alex@gmail.com>
2023-03-28 23:36:07 +0800

    update at 2023-03-28_23:36:07

--- stocks_prices_1min.py
+++ stocks_prices_1min.py
@@ -10,8 +10,14 @@ import pymysql
 import datetime
 
 
-# Define function to get stock prices in real-time
-def stock_prices_realtime(stocks, freq='5m', cnt=2):
+def stock_prices_realtime(stocks, freq='1m', cnt=7):
+    """
+    Get the real-time stock prices for a list of stocks.
+    :param stocks: list of stock codes
+    :param freq: frequency of the stock prices
+    :param cnt: number of stock prices to retrieve
+    :return: pandas dataframe of stock prices
+    """
     stock_datas = []
     for stock in stocks:
         stock_data = Ashare.get_price(stock,frequency=freq,count=cnt)
@@ -20,8 +26,13 @@ def stock_prices_realtime(stocks, freq='5m', cnt=2):
     return pd.concat(stock_datas)
 
 
-# Define function to insert data into MySQL database
 def insert_dataframe_to_mysql(dataframe, table_name, db_config):
+    """
+    Insert a pandas dataframe into a MySQL table.
+    :param dataframe: pandas dataframe to insert
+    :param table_name: name of the MySQL table
+    :param db_config: database configuration dictionary
+    """
     try:
         connection = pymysql.connect(host=db_config['host'],
                                              database=db_config['database'],
@@ -43,14 +54,14 @@ def insert_dataframe_to_mysql(dataframe, table_name, db_config):
         dataframe_columns = list(dataframe)
         columns = ",".join(dataframe_columns)
         values = "VALUES({})".format(",".join(["%s" for _ in dataframe_columns]))
-        insert_statement = "INSERT INTO {} ({}) {}".format(table_name, columns, values)
-        on_duplicate_key_update = " ON DUPLICATE KEY UPDATE " + ", ".join([f"{col} = VALUES({col})" for col in dataframe_columns[2:]])
-        insert_statement += on_duplicate_key_update
+        insert_sql = "INSERT INTO {} ({}) {}".format(table_name, columns, values)
+        on_duplicate_key_update = " ON DUPLICATE KEY UPDATE " + ", ".join([f"{col} = VALUES({col})" for col in dataframe_columns[2:]])         
+        insert_sql += on_duplicate_key_update
         records = dataframe.to_records(index=False)
         insert_records = [tuple(record) for record in records]
-        cursor.executemany(insert_statement, insert_records)
+        cursor.executemany(insert_sql, insert_records)
         connection.commit()
-        print(len(insert_records), "Record inserted successfully into {} table".format(table_name))
+        print(len(insert_records), "Record inserted successfully into {} table".format(table_name))    
     except pymysql.Error as e:
         print("Error while connecting to MySQL", e)
     finally:
@@ -60,10 +71,7 @@ def insert_dataframe_to_mysql(dataframe, table_name, db_config):
             print("MySQL connection is closed")
 
 
-# Get current time and check if it is the first minute of a 5-minute interval
 now = datetime.datetime.now()
-if now.minute%5==1:
-    # Get real-time stock prices
+if now.weekday() < 5 and now.minute%5==1:
     stock_data = stock_prices_realtime(stocks)
-    # Insert stock prices into MySQL database
     insert_dataframe_to_mysql(stock_data, 'stock_prices', db_config)
