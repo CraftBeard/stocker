9277fe9ba0a9ecb9f0c123565ff46ca266025ce8
Alex@Home <lianglx_alex@163.com>
2023-04-05 14:18:18 +0800

    update at 2023-04-05_14:18:17

--- stocker.sh
+++ stocker.sh
@@ -2,6 +2,7 @@
 echo "----- start -----" >> /home/project/stocker/log.txt
 echo `date` "|" stock_meta.py >> /home/project/stocker/log.txt | /home/project/stocker/stocker_env/bin/python /home/project/stocker/stock_meta.py
 echo `date` "|" stock_prices.py `date +%Y-%m-%d` `date +%Y-%m-%d` >> /home/project/stocker/log.txt | /home/project/stocker/stocker_env/bin/python /home/project/stocker/stock_prices.py `date +%Y-%m-%d` `date +%Y-%m-%d`
+echo `date` "|" stock_prices_1min.py >> /home/project/stocker/log.txt | /home/project/stocker/stocker_env/bin/python /home/project/stocker/stock_prices_1min.py
 echo `date` "|" stock_stats.py >> /home/project/stocker/log.txt | /home/project/stocker/stocker_env/bin/python /home/project/stocker/stock_stats.py
 echo `date` "|" stock_email.py >> /home/project/stocker/log.txt | /home/project/stocker/stocker_env/bin/python /home/project/stocker/stock_email.py
 echo "----- end -----" >> /home/project/stocker/log.txt
