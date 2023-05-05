f90df516fe6e9add8292079b66b7179c7c82a09c
Alex@Home <lianglx_alex@163.com>
2023-05-05 22:54:52 +0800

    update at 2023-05-05_22:54:52

--- stock_performance_cninfo.py
+++ stock_performance_cninfo.py
@@ -83,13 +83,15 @@ def upsert_performance(df):
         'F010N': 'gross_profit_margin',
     }
     df_upsert = df.rename(columns=rename_cols)
+    df_upsert = df_upsert.fillna(-1)
+    df_upsert = df_upsert[['report_date', 'code', 'name', 'holder_net_income', 'operate_income', 'operate_profit', 'operate_cost', 'operate_share_flow', 'share_net_asset', 'share_earn', 'roe', 'operate_margin', 'gross_profit_margin']]
     cnx = pymysql.connect(host=db_config['host'], user=db_config['user'], password=db_config['password'], database=db_config['database'])
     cursor = cnx.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS stock_performance (
             report_date date not null comment '报告期',
-            code varchar not null comment '证券代码',
-            name varchar comment '证券简称',
+            code varchar(30) not null comment '证券代码',
+            name varchar(30) comment '证券简称',
             holder_net_income float comment '归属母公司所有者净利润(元)',
             operate_income float comment '营业收入(元)',
             operate_profit float comment '营业利润(元)',
@@ -104,7 +106,7 @@ def upsert_performance(df):
         )
     """)
     # Upsert the data frame into the stock_performance table
-    cursor.execute("""
+    cursor.executemany("""
         INSERT INTO stock_performance (
             report_date, 
             code, 
@@ -137,7 +139,7 @@ def upsert_performance(df):
     cnx.commit()
     return "Succeed"
 
-def stock_forcast(url='http://www.cninfo.com.cn/data20/performanceForecast/list?rdate=20230331', data={}):
+def stock_forcast(url='http://www.cninfo.com.cn/data20/performanceForecast/list?rdate=20230331'):
     headers = {
         'Content-Type': 'application/json; charset=utf-8', 
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
@@ -154,10 +156,7 @@ def stock_forcast(url='http://www.cninfo.com.cn/data20/performanceForecast/list?
         'Cookie': 'Hm_lvt_489bd07e99fbfc5f12cbb4145adb0a9b={time_stamp}; Hm_lpvt_489bd07e99fbfc5f12cbb4145adb0a9b={time_stamp}'.format(time_stamp = int(time.time())),
         'mcode': 'MTY4MzEzMTYyMw==',
     }
-    print(url)
-    print(headers)
-    res = rq.post(url, headers, json=data)
-    print(res)
+    res = rq.post(url, headers)
     res_json = json.loads(res.text)
     return {'json':res_json, 'text':res}
 
@@ -175,26 +174,26 @@ def upsert_forcast(df):
         'F009N': 'net_profit_lower',
     }
     df_upsert = df.rename(columns=rename_cols)
-
+    df_upsert = df_upsert.fillna(-1)
+    df_upsert = df_upsert[['report_date', 'code', 'name', 'industry_2nd', 'perform_type', 'forcast_content', 'perform_reason', 'net_profit_upper', 'net_profit_lower', 'update_date']]
     cnx = pymysql.connect(host=db_config['host'], user=db_config['user'], password=db_config['password'], database=db_config['database'])
     cursor = cnx.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS stock_forcast (
             report_date date not null comment '报告年度',
-            code varchar not null comment '证券代码',
-            name varchar comment '证券简称',
-            industry_2nd varchar comment '二级行业',
-            perform_type varchar comment '业绩变动类型',
-            forcast_content varchar comment '业绩变动原因',
-            perform_reason varchar comment '业绩变动幅度',
+            code varchar(30) not null comment '证券代码',
+            name varchar(30) comment '证券简称',
+            industry_2nd varchar(30) comment '二级行业',
+            perform_type varchar(30) comment '业绩变动类型',
+            forcast_content varchar(1000) comment '业绩预告内容',
+            perform_reason varchar(1000) comment '业绩变动原因',
             net_profit_upper float comment '净利润变动幅度上限(%)',
             net_profit_lower float comment '净利润变动幅度下限(%)',
             update_date date comment '发布日期',
             PRIMARY KEY (report_date, code)
         )
     """)
-
-    cursor.execute("""
+    cursor.executemany("""
         INSERT INTO stock_forcast (
             report_date, 
             code, 
@@ -227,7 +226,9 @@ df_perform_sh = pd.DataFrame(stock_perform_sh['json']['records'])
 upsert_performance(df_perform_sh)
 stock_perform_sz = stock_performance(params={'report_date': REPORT_DATE.replace('-',''), 'market': '012002'})
 df_perform_sz = pd.DataFrame(stock_perform_sz['json']['records'])
+upsert_performance(df_perform_sz)
 
 # 业绩预告
 stock_forcast = stock_forcast(url='http://www.cninfo.com.cn/data20/performanceForecast/list?rdate={}'.format(REPORT_DATE.replace('-','')))
 df_forcast = pd.DataFrame(stock_forcast['json']['data']['records'])
+upsert_forcast(df_forcast)
