891fa15b40330a10dff239015c1eae56fcebd1a3
Alex@Home <lianglx_alex@163.com>
2023-05-05 23:36:47 +0800

    update at 2023-05-05_23:36:46

--- cninfo.py
+++ cninfo.py
@@ -8,7 +8,7 @@ cnx = pymysql.connect(host=db_config['host'], user=db_config['user'], password=d
 cursor = cnx.cursor()
 
 stock_performance = pd.read_sql_query("SELECT * FROM stock_performance", cnx)
-stock_forcase = pd.read_sql_query("SELECT * FROM stock_forcase", cnx)
+stock_forcast = pd.read_sql_query("SELECT * FROM stock_forcast", cnx)
 
 stock_performance.to_csv('业绩报告.csv')
-stock_forcase.to_csv('业绩预告.csv')
+stock_forcast.to_csv('业绩预告.csv')
